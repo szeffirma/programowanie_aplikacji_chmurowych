a42b6c7bf0ce7c14a47b28b8c33c41cffee94051
Kahivandekell <chrostowskialekss@gmail.com>
2026-05-16 14:09:15 +0200

    [001] make app

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/{*path}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Serwer działa na porcie ${port}`);
+});
