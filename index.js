6df969553148a1f4c7be602c671864eafdc19dca
Kahivandekell <chrostowskialekss@gmail.com>
2026-05-16 18:27:39 +0200

    [020] hotfix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/api', createProxyMiddleware({
 
 app.use(express.static(path.join(__dirname, 'client-build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client-build', 'index.html'));
 });
 
