f5c5b218e75b5769bcf1b1cce7e159d105b68908
szeffirma <fifaft79@gmail.com>
2026-06-13 19:27:47 +0200

    [033] ostatnie...?

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('your_app.urls')), # Twoje API
+    path('api/', include('liga.urls')), # Twoje API
     
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
